d62bda118570ec5188d1f8defca4d4d38014a0a8
Rafaela Bárbara Custódio <rafa.b.custodio13@gmail.com>
2018-12-05 23:59:51 -0200

    identacao

--- sketch/hash.hpp
+++ sketch/hash.hpp
@@ -171,186 +171,186 @@ class TabelaHash { //Class TabelaHash
           mapa2.close();
         } else
           Serial << "Arquivo Vazio";
-      }else {
-      Serial << "Arquivo Vazio";
+      } else {
+        Serial << "Arquivo Vazio";
+      }
     }
-}
-void imprimirOrdenado(int bloco);
-void remover(int id){
-    int h;
-    h = funcaoHash(id, altura);
-    int *vet = new int[altura];
-    vet = intToBin(h);
-    int posRemocao = 0;
+    void imprimirOrdenado(int bloco);
+    void remover(int id) {
+      int h;
+      h = funcaoHash(id, altura);
+      int *vet = new int[altura];
+      vet = intToBin(h);
+      int posRemocao = 0;
 
-    for (int i = 0; i < altura; i++)
-    {
+      for (int i = 0; i < altura; i++)
+      {
         if (vet[i] == 0)
         {
-            posRemocao += esquerda(posRemocao);
+          posRemocao += esquerda(posRemocao);
         }
 
         else
         {
-            posRemocao += direita(posRemocao);
+          posRemocao += direita(posRemocao);
         }
-    }
-    int *posicaoArquivo = new int;
-    *posicaoArquivo = posRemocao * sizeof(Bloco);
-    File mapa = SD.open("mapeamento.txt");
-    bool sinalizador = false;
-    int *posEmBytes = new int;
-    File user = SD.open("info.txt");
-    Bloco *blocoHash = new Bloco;
-    user.seek(*posicaoArquivo);
-    user.read((char *)blocoHash, sizeof(Bloco));
-    char null[1];
-    null[0] = '%';
-    if (blocoHash->vetor[0].mId == id || blocoHash->vetor[1].mId == id || blocoHash->vetor[2].mId == id || blocoHash->vetor[3].mId == id){
-        for (int i = 0; i < 4; i++){
-            if (blocoHash->vetor[i].mId == id){  
-                blocoHash->vetor[i].mId = -1;
-                blocoHash->vetor[i].mMatricula = -1;
-            }
+      }
+      int *posicaoArquivo = new int;
+      *posicaoArquivo = posRemocao * sizeof(Bloco);
+      File mapa = SD.open("mapeamento.txt");
+      bool sinalizador = false;
+      int *posEmBytes = new int;
+      File user = SD.open("info.txt");
+      Bloco *blocoHash = new Bloco;
+      user.seek(*posicaoArquivo);
+      user.read((char *)blocoHash, sizeof(Bloco));
+      char null[1];
+      null[0] = '%';
+      if (blocoHash->vetor[0].mId == id || blocoHash->vetor[1].mId == id || blocoHash->vetor[2].mId == id || blocoHash->vetor[3].mId == id) {
+        for (int i = 0; i < 4; i++) {
+          if (blocoHash->vetor[i].mId == id) {
+            blocoHash->vetor[i].mId = -1;
+            blocoHash->vetor[i].mMatricula = -1;
+          }
         }
         blocoHash->bTamanho--;
         user.seek(*posicaoArquivo);
         user.write((char *)blocoHash, sizeof(Bloco));
         int outrocontador = 0;
-        if (blocoHash->bTamanho == 0){
-            int *inatividade = new int;
-            *inatividade = -1;
-            while (!mapa.available() and !sinalizador)
-            {
-                outrocontador++;
-                mapa.read((char *)posEmBytes, sizeof(int));
-                if (*posicaoArquivo == *posEmBytes){                
-                    mapa.seek((outrocontador - 1) * sizeof(int));
-                    mapa.write((char *)inatividade, sizeof(int));
-                    sinalizador = true;
-                }
-               
+        if (blocoHash->bTamanho == 0) {
+          int *inatividade = new int;
+          *inatividade = -1;
+          while (!mapa.available() and !sinalizador)
+          {
+            outrocontador++;
+            mapa.read((char *)posEmBytes, sizeof(int));
+            if (*posicaoArquivo == *posEmBytes) {
+              mapa.seek((outrocontador - 1) * sizeof(int));
+              mapa.write((char *)inatividade, sizeof(int));
+              sinalizador = true;
             }
-        }
-    }else{
-        Serial << "ID NÃO ENCONTRADO";
-    }
-    mapa.close();
-    user.close();
-}
-bool verificaChave(int h) {
-  File entradaMapa =  SD.open("mapeamento.txt");
-  int *ph;
-  ph = new int;
-  bool indicador = false;
-  if (entradaMapa)
-  {
-    while (!entradaMapa.available() && indicador == false)
-    {
-      entradaMapa.read((char *)ph, sizeof(int));
-      if (h == *ph)
-      {
-        indicador = true;
-      }
-    }
-  }
-  entradaMapa.close();
-  return indicador;
-}
-bool verificaChaveAux(int h);
-bool verificaId(Bloco* blocoHash, int id);
-int* intToBin(int c) {
-  int *binVet = new int[altura];
-  for (int i = altura - 1; i >= 0; i--) {
-    binVet[i] = c % 2;
-    c /= 2;
-  }
-  return binVet;
-}
-Bloco* busca(int c) {
-  int h;
-  h = funcaoHash(c, altura);
-  int *vet = new int[altura];
-  vet = intToBin(h);
-  int posInsercao = 0;
-  for (int i = 0; i < altura; i++) {
-    if (vet[i] == 0) {
-      posInsercao += esquerda(posInsercao);
-    } else {
-      posInsercao += direita(posInsercao);
-    }
-  }
-  int posicaoArquivo;
-  posicaoArquivo = posInsercao * sizeof(Bloco);
-  Bloco *blocoHash = new Bloco;
-  File user = SD.open("info.txt");
-  user.seek(posicaoArquivo /*, user.beg*/);
-  user.read((char *)blocoHash, sizeof(Bloco));
-  for (int i = 0; i < 4; i++) {
-    if (blocoHash->vetor[i].mId == c) {
-      return blocoHash;
-    }
-  }
-  return NULL;
-}
-void imprimeBuscado(int c) {
-  Bloco* buscado = new Bloco;
-  buscado = busca(c);
-  if (buscado == NULL) {
-    Serial << "ID não encontrado!";
-  } else {
-    for (int cont = 0; cont < 4; cont++) {
-      if (buscado->vetor[cont].mId != -1) {
-        if (buscado->vetor[cont].mId == c) {
-          Serial << "Id: " <<  buscado->vetor[cont].mId ;
-          Serial << "Matricula: " <<  buscado->vetor[cont].mMatricula;
-        }
-      }
-    }
-  }
-}
-int esquerda(int posicao) {
-  posicao = (posicao * 2) + 1;
-  return posicao;
-}
-int direita(int posicao) {
-  posicao = (posicao * 2) + 2;
-  return posicao;
-}
-void redimensiona() {
-  File user = SD.open("cerrado.txt");
-  File mapa = SD.open("mapeamento.txt");
-  mapa.seek(0/*, mapa.end*/);
-  int tam = mapa.position();
-  mapa.seek(0/*, mapa.beg*/);
-  altura++;
-  int *posEmBytes = new int;
-  Bloco *blocoHash = new Bloco;
-  for (unsigned int i = 0; i < (tam / sizeof(int)); i++)
-  {
-    mapa.read((char *)posEmBytes, sizeof(int));
-    user.seek(*posEmBytes);
-    user.read((char *)blocoHash, sizeof(Bloco));
-    for (int j = 0; j < 4; j++)
-    {
-      if (blocoHash->vetor[j].mId != -1)
-      {
 
-        insereAux(blocoHash->vetor[j].mId, blocoHash->vetor[j].mMatricula);
+          }
+        }
+      } else {
+        Serial << "ID NÃO ENCONTRADO";
+      }
+      mapa.close();
+      user.close();
+    }
+    bool verificaChave(int h) {
+      File entradaMapa =  SD.open("mapeamento.txt");
+      int *ph;
+      ph = new int;
+      bool indicador = false;
+      if (entradaMapa)
+      {
+        while (!entradaMapa.available() && indicador == false)
+        {
+          entradaMapa.read((char *)ph, sizeof(int));
+          if (h == *ph)
+          {
+            indicador = true;
+          }
+        }
+      }
+      entradaMapa.close();
+      return indicador;
+    }
+    bool verificaChaveAux(int h);
+    bool verificaId(Bloco* blocoHash, int id);
+    int* intToBin(int c) {
+      int *binVet = new int[altura];
+      for (int i = altura - 1; i >= 0; i--) {
+        binVet[i] = c % 2;
+        c /= 2;
+      }
+      return binVet;
+    }
+    Bloco* busca(int c) {
+      int h;
+      h = funcaoHash(c, altura);
+      int *vet = new int[altura];
+      vet = intToBin(h);
+      int posInsercao = 0;
+      for (int i = 0; i < altura; i++) {
+        if (vet[i] == 0) {
+          posInsercao += esquerda(posInsercao);
+        } else {
+          posInsercao += direita(posInsercao);
+        }
+      }
+      int posicaoArquivo;
+      posicaoArquivo = posInsercao * sizeof(Bloco);
+      Bloco *blocoHash = new Bloco;
+      File user = SD.open("info.txt");
+      user.seek(posicaoArquivo /*, user.beg*/);
+      user.read((char *)blocoHash, sizeof(Bloco));
+      for (int i = 0; i < 4; i++) {
+        if (blocoHash->vetor[i].mId == c) {
+          return blocoHash;
+        }
+      }
+      return NULL;
+    }
+    void imprimeBuscado(int c) {
+      Bloco* buscado = new Bloco;
+      buscado = busca(c);
+      if (buscado == NULL) {
+        Serial << "ID não encontrado!";
+      } else {
+        for (int cont = 0; cont < 4; cont++) {
+          if (buscado->vetor[cont].mId != -1) {
+            if (buscado->vetor[cont].mId == c) {
+              Serial << "Id: " <<  buscado->vetor[cont].mId ;
+              Serial << "Matricula: " <<  buscado->vetor[cont].mMatricula;
+            }
+          }
+        }
       }
     }
-  }
-  File modAltura =  SD.open("alt.txt", FILE_WRITE);
-  int *altWrite = new int;
-  *altWrite = altura;
-  modAltura.write((char *)altWrite, sizeof(char));
-  SD.remove("info.txt");
-  SD.remove("mapeamento.txt");
-  rename("infoAux.txt", "info.txt");
-  rename("mapeamentoAux.txt", "mapeamento.txt");
-  user.close();
-  mapa.close();
-}
+    int esquerda(int posicao) {
+      posicao = (posicao * 2) + 1;
+      return posicao;
+    }
+    int direita(int posicao) {
+      posicao = (posicao * 2) + 2;
+      return posicao;
+    }
+    void redimensiona() {
+      File user = SD.open("cerrado.txt");
+      File mapa = SD.open("mapeamento.txt");
+      mapa.seek(0/*, mapa.end*/);
+      int tam = mapa.position();
+      mapa.seek(0/*, mapa.beg*/);
+      altura++;
+      int *posEmBytes = new int;
+      Bloco *blocoHash = new Bloco;
+      for (unsigned int i = 0; i < (tam / sizeof(int)); i++)
+      {
+        mapa.read((char *)posEmBytes, sizeof(int));
+        user.seek(*posEmBytes);
+        user.read((char *)blocoHash, sizeof(Bloco));
+        for (int j = 0; j < 4; j++)
+        {
+          if (blocoHash->vetor[j].mId != -1)
+          {
+
+            insereAux(blocoHash->vetor[j].mId, blocoHash->vetor[j].mMatricula);
+          }
+        }
+      }
+      File modAltura =  SD.open("alt.txt", FILE_WRITE);
+      int *altWrite = new int;
+      *altWrite = altura;
+      modAltura.write((char *)altWrite, sizeof(char));
+      SD.remove("info.txt");
+      SD.remove("mapeamento.txt");
+      rename("infoAux.txt", "info.txt");
+      rename("mapeamentoAux.txt", "mapeamento.txt");
+      user.close();
+      mapa.close();
+    }
 
 
 };
