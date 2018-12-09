1662dadb4ab78a78b2a4db142bcd5b1e30c7d23e
Rafaela Bárbara Custódio <rafa.b.custodio13@gmail.com>
2018-12-09 15:30:05 -0200

    inserindo (falta arrumar insercao com msm chave), buscando ok, listando ok, falta ordenacao e remover

--- sketch.ino
+++ sketch.ino
@@ -11,32 +11,46 @@
   #define RST_PIN 9
 
   MFRC522 mfrc522(SS_PIN, RST_PIN);*/
+TabelaHash objeto(16);
 void setup() {
   Serial.begin(9600); // Inicializa a comunicação serial com uma taxa de 9600 bauds.
   Serial.print("Inicializa SD card...");
-      if (!SD.begin(4)) {
-        Serial.println("inicializacao falhou!");
-        while (1);
-      }
- Serial.println("Inicializado!.");
-
+  if (!SD.begin(4)) {
+    Serial.println("inicializacao falhou!");
+    while (1);
+  }
+  Serial.println("Inicializado!.");
+  objeto.percorre();
 }
 
 void loop() {
-  TabelaHash objeto(16);
+
   int id = 0; // deveria ser o codigo do cartão rfid mas não conseguimos utilizar ele junto com o cartão SD
   int matricula = 0;
-
-  if (Serial.available() > 0){
-    id = Serial.parseInt();
-  }
-  if (Serial.available() > 0){
-    matricula = Serial.parseInt();
-  }
-
-  if(matricula != 0 and id != 0){
-    objeto.insere(id, matricula);
-    objeto.percorre();
-  }
+  int busca = 0;
   
+  if (Serial.available() > 0) {
+    Serial << "Informe id para nova insercao:";
+    id = Serial.parseInt();
+
+  }
+  if (Serial.available() > 0) {
+    Serial<<"Informe matricula para nova insercao: ";
+    matricula = Serial.parseInt();
+
+
+  }
+  if (matricula != 0 and id != 0) {
+    objeto.insere(id, matricula);
+    Serial << "Inserido com sucesso!";
+  }
+  if (Serial.available() > 0) {
+    Serial <<"Informe id para buscar:";
+      busca = Serial.parseInt();
+    }
+    if (busca != 0) {
+      objeto.consulta(busca);
+    }
+
+
 }
